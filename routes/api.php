79267140a19013f128df5bed358f3addc949e169
wilson <wilsonjp.dev@outlook.com>
2022-06-08 12:09:47 -0300

    Routes created

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cliente', \App\Http\Controllers\ClienteController::class);
+Route::apiResource('carro', \App\Http\Controllers\CarroController::class);
+Route::apiResource('locacao', \App\Http\Controllers\LocacaoController::class);
+Route::apiResource('marca', \App\Http\Controllers\MarcaController::class);
+Route::apiResource('modelo', \App\Http\Controllers\ModeloController::class);
